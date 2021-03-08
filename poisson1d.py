756a0f953a22b0fb1e9a6fe4a41721e85e37f243
antelk <alojic00@fesb.hr>
2021-03-08 10:37:09 +0100

    rename

--- poisson1d.py
+++ poisson1d.py
@@ -25,36 +25,10 @@ import torch
 from torch import nn
 import tqdm
 
-
-SUPPORTED_OPTIMIZERS = ['bfgs', 'sgd', 'adam']
-
-
-def optimizer_dispatcher(optimizer, parameters, learning_rate):
-    r"""Return optimization function from `SUPPORTED_OPTIMIZERS`.
-
-    Parameters
-    ----------
-    optimizer : str
-        Optimization function name
-    parameters : callable
-        Network parameters
-    learning_rate : float
-        Learning rate
-
-    Returns
-    -------
-    callable
-        Optimization function
-    """
-    assert isinstance(optimizer, (str, )), '`optimizer` type must be str.'
-    optimizer = optimizer.lower()
-    assert optimizer in SUPPORTED_OPTIMIZERS, 'Invalid optimizer. Falling to default.'
-    if optimizer == 'bfgs':
-        return torch.optim.LBFGS(parameters, line_search_fn="strong_wolfe")
-    elif optimizer == 'sgd':
-        return torch.optim.SGD(parameters, lr=learning_rate, momentum=0.9, nesterov=True, weight_decay=1e-2*learning_rate)
-    else:
-        return torch.optim.Adam(parameters, lr=learning_rate, betas=(0.9, 0.999), eps=1e-5)
+from utils import SUPPORTED_OPTIMIZERS
+from utils import optimizer_dispatcher
+from utils import Swish
+from utils import AdaptiveLinear
 
 
 def parse_arguments():
@@ -110,7 +84,9 @@ def parse_arguments():
 
 def solve_poisson(x, rhs, boundary_conditions):
     r"""Solve 1-D Poisson equation of simple form as follows:
-    :math:`\frac{\mathrm{d} \phi^2}{\mathrm{d} x^2} = f(x)`
+    .. math::
+        \frac{\mathrm{d} \phi^2}{\mathrm{d} x^2} = f(x)
+
     with known Dirichlet boundary conditions on arbitrary solution
     domain.
 
@@ -137,50 +113,6 @@ def solve_poisson(x, rhs, boundary_conditions):
     C2 = boundary_conditions[0] - rhs / 2 * x0**2 - C1 * x0 
     return rhs / 2 * x**2 + C1 * x + C2
 
-
-class AdaptiveLinear(nn.Linear):
-    r"""Applies a linear transformation to the input data as follows
-    :math:`y = naxA^T + b`.
-    More details available in Jagtap, A. D. et al. Locally adaptive
-    activation functions with slope recovery for deep and
-    physics-informed neural networks, Proc. R. Soc. 2020.
-
-    Parameters
-    ----------
-    in_features : int
-        The size of each input sample
-    out_features : int 
-        The size of each output sample
-    bias : bool, optional
-        If set to ``False``, the layer will not learn an additive bias
-    adaptive_rate : float, optional
-        Scalable adaptive rate parameter for activation function that
-        is added layer-wise for each neuron separately. It is treated
-        as learnable parameter and will be optimized using a optimizer
-        of choice
-    adaptive_rate_scaler : float, optional
-        Fixed, pre-defined, scaling factor for adaptive activation
-        functions
-    """
-    def __init__(self, in_features, out_features, bias=True, adaptive_rate=None, adaptive_rate_scaler=None):
-        super(AdaptiveLinear, self).__init__(in_features, out_features, bias)
-        self.adaptive_rate = adaptive_rate
-        self.adaptive_rate_scaler = adaptive_rate_scaler
-        if self.adaptive_rate:
-            self.A = nn.Parameter(self.adaptive_rate * torch.ones(self.in_features))
-            if not self.adaptive_rate_scaler:
-                self.adaptive_rate_scaler = 10.0
-            
-    def forward(self, input):
-        if self.adaptive_rate:
-            return nn.functional.linear(self.adaptive_rate_scaler * self.A * input, self.weight, self.bias)
-        return nn.functional.linear(input, self.weight, self.bias)
-
-    def extra_repr(self):
-        return (
-            f'in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}, '
-            f'adaptive_rate={self.adaptive_rate is not None}, adaptive_rate_scaler={self.adaptive_rate_scaler is not None}'
-        )
 
 class Net(nn.Module):
     r"""Neural approximator for the unknown function that is supposed
@@ -228,6 +160,7 @@ class Net(nn.Module):
             ['relu', nn.ReLU()],
             ['tanh', nn.Tanh()],
             ['sigmoid', nn.Sigmoid()],
+            ['swish', Swish()]
         ])
         return nn.Sequential(
             AdaptiveLinear(in_features, out_features, adaptive_rate=adaptive_rate, adaptive_rate_scaler=adaptive_rate_scaler),
